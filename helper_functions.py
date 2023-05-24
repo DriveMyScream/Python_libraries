f3aad46257c27fcf4b0ef374cb325e74696ab41c
Shubham Pilivkar <shubhampilivkar@gmail.com>
2023-05-24 19:34:54 +0530

    Add files via upload

---
+++ helper_functions.py
@@ -0,0 +1,202 @@
+### We create a bunch of helpful functions throughout the course.
+### Storing them here so they're easily accessible.
+
+import tensorflow as tf
+from tensorflow.keras.callbacks import TensorBoard
+import os
+# Note: The following confusion matrix code is a remix of Scikit-Learn's 
+# plot_confusion_matrix function - https://scikit-learn.org/stable/modules/generated/sklearn.metrics.plot_confusion_matrix.html
+import itertools
+import datetime
+import matplotlib.pyplot as plt
+import random
+import numpy as np
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import accuracy_score, precision_recall_fscore_support
+
+# Our function needs a different name to sklearn's plot_confusion_matrix
+def make_confusion_matrix(y_true, y_pred, classes=None, figsize=(10, 10), text_size=15, norm=False, savefig=False): 
+  """Makes a labelled confusion matrix comparing predictions and ground truth labels.
+
+  If classes is passed, confusion matrix will be labelled, if not, integer class values
+  will be used.
+
+  Args:
+    y_true: Array of truth labels (must be same shape as y_pred).
+    y_pred: Array of predicted labels (must be same shape as y_true).
+    classes: Array of class labels (e.g. string form). If `None`, integer labels are used.
+    figsize: Size of output figure (default=(10, 10)).
+    text_size: Size of output figure text (default=15).
+    norm: normalize values or not (default=False).
+    savefig: save confusion matrix to file (default=False).
+  
+  Returns:
+    A labelled confusion matrix plot comparing y_true and y_pred.
+
+  Example usage:
+    make_confusion_matrix(y_true=test_labels, # ground truth test labels
+                          y_pred=y_preds, # predicted labels
+                          classes=class_names, # array of class label names
+                          figsize=(15, 15),
+                          text_size=10)
+  """  
+  # Create the confustion matrix
+  cm = confusion_matrix(y_true, y_pred)
+  cm_norm = cm.astype("float") / cm.sum(axis=1)[:, np.newaxis] # normalize it
+  n_classes = cm.shape[0] # find the number of classes we're dealing with
+
+  # Plot the figure and make it pretty
+  fig, ax = plt.subplots(figsize=figsize)
+  cax = ax.matshow(cm, cmap=plt.cm.Blues) # colors will represent how 'correct' a class is, darker == better
+  fig.colorbar(cax)
+
+  # Are there a list of classes?
+  if classes:
+    labels = classes
+  else:
+    labels = np.arange(cm.shape[0])
+  
+  # Label the axes
+  ax.set(title="Confusion Matrix",
+         xlabel="Predicted label",
+         ylabel="True label",
+         xticks=np.arange(n_classes), # create enough axis slots for each class
+         yticks=np.arange(n_classes), 
+         xticklabels=labels, # axes will labeled with class names (if they exist) or ints
+         yticklabels=labels)
+  
+  # Make x-axis labels appear on bottom
+  ax.xaxis.set_label_position("bottom")
+  ax.xaxis.tick_bottom()
+
+  # Set the threshold for different colors
+  threshold = (cm.max() + cm.min()) / 2.
+
+  # Plot the text on each cell
+  for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+    if norm:
+      plt.text(j, i, f"{cm[i, j]} ({cm_norm[i, j]*100:.1f}%)",
+              horizontalalignment="center",
+              color="white" if cm[i, j] > threshold else "black",
+              size=text_size)
+    else:
+      plt.text(j, i, f"{cm[i, j]}",
+              horizontalalignment="center",
+              color="white" if cm[i, j] > threshold else "black",
+              size=text_size)
+
+  # Save the figure to the current working directory
+  if savefig:
+    fig.savefig("confusion_matrix.png")
+  
+  
+
+
+def plot_loss_curves(history):
+    
+    loss = np.array(history.history['loss'])
+    val_loss = np.array(history.history['val_loss'])
+
+    accuracy = np.array(history.history['accuracy'])
+    val_accuracy = np.array(history.history['val_accuracy'])
+
+    epochs = range(len(history.history['loss']))
+
+    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(20, 10))
+
+    # Plot loss
+    ax1.plot(epochs, loss, label='training_loss', marker='o')
+    ax1.plot(epochs, val_loss, label='val_loss', marker='o')
+    
+    ax1.fill_between(epochs, loss, val_loss, where=(loss > val_loss), color='C0', alpha=0.3, interpolate=True)
+    ax1.fill_between(epochs, loss, val_loss, where=(loss < val_loss), color='C1', alpha=0.3, interpolate=True)
+
+    ax1.set_title('Loss (Lower Means Better)', fontsize=16)
+    ax1.set_xlabel('Epochs', fontsize=12)
+    ax1.legend()
+
+    # Plot accuracy
+    ax2.plot(epochs, accuracy, label='training_accuracy', marker='o')
+    ax2.plot(epochs, val_accuracy, label='val_accuracy', marker='o')
+    
+    ax2.fill_between(epochs, accuracy, val_accuracy, where=(accuracy > val_accuracy), color='C0', alpha=0.3, interpolate=True)
+    ax2.fill_between(epochs, accuracy, val_accuracy, where=(accuracy < val_accuracy), color='C1', alpha=0.3, interpolate=True)
+
+    ax2.set_title('Accuracy (Higher Means Better)', fontsize=16)
+    ax2.set_xlabel('Epochs', fontsize=12)
+    ax2.legend();
+
+
+    
+# Function to evaluate: accuracy, precision, recall, f1-score
+
+def calculate_results(y_true, y_pred):
+  """
+  Calculates model accuracy, precision, recall and f1 score of a binary classification model.
+
+  Args:
+      y_true: true labels in the form of a 1D array
+      y_pred: predicted labels in the form of a 1D array
+
+  Returns a dictionary of accuracy, precision, recall, f1-score.
+  """
+  # Calculate model accuracy
+  model_accuracy = accuracy_score(y_true, y_pred) * 100
+  # Calculate model precision, recall and f1 score using "weighted average
+  model_precision, model_recall, model_f1, _ = precision_recall_fscore_support(y_true, y_pred, average="weighted")
+  model_results = {"accuracy": model_accuracy,
+                  "precision": model_precision,
+                  "recall": model_recall,
+                  "f1": model_f1}
+  return model_results
+
+
+def plot_class_accuracy(num_classes, true_labels, predicted_labels, class_names):
+  class_accuracy = []
+  for class_label in range(num_classes):
+    class_indices = np.where(true_labels == class_label)
+    class_true_labels = true_labels[class_indices]
+    class_predicted_labels = predicted_labels[class_indices]
+    class_accuracy.append(np.mean(class_true_labels == class_predicted_labels))
+
+  # Plotting the bar chart
+  plt.figure(figsize=(10, 6))
+  plt.bar(range(num_classes), class_accuracy)
+  plt.xticks(range(num_classes), class_names)
+  plt.xlabel('Class Label')
+  plt.ylabel('Accuracy')
+  plt.title('Class-wise Accuracy')
+  plt.show();
+   
+
+def plot_random_misclassified_images(true_labels, predicted_labels, images, class_names):
+  result = np.absolute(true_labels - predicted_labels)
+  misclassified_images = np.where(result > 0)[0]
+    
+  random_indices = random.sample(list(misclassified_images), k=25)
+
+  plt.figure(figsize=(12, 12))
+  for i, image_index in enumerate(random_indices):
+    plt.subplot(5, 5, i + 1, xticks=[], yticks=[])
+    image_data = images[image_index]
+    plt.imshow(image_data/255)
+    plt.title(f'True: {class_names[true_labels[image_index]]}\nPredicted: {class_names[predicted_labels[image_index]]}')
+    plt.axis('off')
+  plt.tight_layout()
+  plt.show();
+    
+def plot_prediction_confidence_histogram(predictions):
+  
+  max_confidence = np.max(predictions, axis=1)
+  plt.figure(figsize=(10, 6))
+  plt.hist(max_confidence, bins=10, edgecolor='black')
+  plt.xlabel('Maximum Confidence')
+  plt.ylabel('Frequency')
+  plt.title('Prediction Confidence Histogram')
+  plt.show();
+
+
+def create_tensorboard_callback(model_name):
+    log_dir = os.path.join("logs", model_name + "_" + datetime.now().strftime("%Y%m%d-%H%M%S"))
+    tensorboard_callback = TensorBoard(log_dir=log_dir)
+    return tensorboard_callback
